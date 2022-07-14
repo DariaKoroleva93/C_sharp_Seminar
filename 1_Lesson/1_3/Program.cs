abae9146a38b87cbb9253de37ad63b74d4906fe8
Daria <koroleva_daria_93@mail.ru>
2022-07-14 20:07:47 +0300

    Task 1_3

---
+++ 1_Lesson/1_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Дни недели
+Console.WriteLine("Write a number: ");
+int a = int.Parse(Console.ReadLine());
+
+
+if (a == 1)
+    Console.WriteLine("Mond");
+
+else if(a == 2)
+  {  Console.WriteLine("Tue");
+  }
+else if(a == 3)
+   { Console.WriteLine("Wed");
+   }
+else if(a == 4) 
+    {Console.WriteLine("Th");
+    }
+    
+else if(a == 5)
+    {Console.WriteLine("Fr");
+    }
+    
+else if(a == 6)
+    {Console.WriteLine("Sa");
+    }
+    
+else if(a == 7)
+  {  Console.WriteLine("Su");
+  }
+else 
+{
+Console.WriteLine("Error");
+
+}
